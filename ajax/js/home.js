2ba461a8d5aae98fbf2a4eab8b366cb90c422ff0
thanhtr297 <thanh.tranminh.hanoi@gmail.com>
2023-10-06 16:30:18 +0700

    hop nhat FE BE

--- ajax/js/home.js
+++ ajax/js/home.js
@@ -1,3 +1,206 @@
+let arrHome;
+let listDisplayPage;
+let numberPage;
+let totalPage;
+function demoDisplay() {
+    $.ajax({
+        url: "http://localhost:8080/api/homes",
+        type: "GET",
+        success: function (data) {
+            showHome(data)
+            numberPage = 0;
+            arrHome = data;
+            listDisplayPage = data.reverse();
+            showPage();
+        }
+    })
+
+}
+function detailHome(id) {
+    $.ajax({
+        url: `http://localhost:8080/api/homes/${id}`,
+        type:"GET"
+    })
+}
+
+function findOne() {
+
+}
+
+function Filter() {
+    let price = $("#price").val();
+    let priceValue = price.split("-")
+    let minPrice = priceValue[0];
+    let maxPrice = priceValue[1];
+    let count_bathroom = $("#bathroom").val();
+    let count_bedroom = $("#bedroom").val();
+    let idCity = $('#select_city').val();
+    if(idCity === "--Chọn thành phố--") {
+        idCity = null;
+    }
+    let idDistrict = $('#select_district').val();
+    if(idDistrict === undefined) {
+        idDistrict = null;
+    }
+    let idStatus = $('#select_status').val();
+    if(idStatus === "--Chọn trạng thái--") {
+        idStatus = null;
+    }
+    newFilter = {
+        minPrice: minPrice,
+        maxPrice: maxPrice,
+        count_bathroom: count_bathroom,
+        count_bedroom: count_bedroom,
+        city: {
+            idCity: idCity
+        },
+        address: {
+            idAddress: idDistrict
+        },
+        status: {
+            idStatus: idStatus
+        }
+    }
+
+    $.ajax({
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        type: "POST",
+        data: JSON.stringify(newFilter),
+        url: "http://localhost:8080/api/filters",
+        success: function (arr) {
+            if (arr == null) {
+                document.getElementById("homes").innerHTML = "Khong tim thay"
+            } else {
+                showHome(arr);
+                showPage();
+            }
+        }
+    });
+    event.preventDefault();
+}
+
+function searchByName() {
+    let search = document.getElementById("search").value
+    $.ajax({
+        url: `http://localhost:8080/api/homes/search/${search}`,
+        type: "GET",
+        success: function (data) {
+           showHome(data);
+            showPage();
+        }
+    })
+    event.preventDefault();
+}
+function displayOneImg(id) {
+    var settings = {
+        "url": `http://localhost:8080/api/homes/img/${id}`,
+        "method": "GET",
+        "timeout": 0,
+    };
+
+    $.ajax(settings).done(function (response) {
+        let content = "";
+        for (let i = 0; i < response.length; i++) {
+            content += `<img style="width: 100px" src="../../src/main/resources/static/image/${response[i].image}" alt=""/>`
+            break;
+        }
+        document.getElementById("img" + id).innerHTML = content;
+    });
+}
+
+// phan trang
+
+
+function showPage() {
+    let data = listDisplayPage;
+    let elementPage = 2;
+    totalPage = Math.ceil(data.length / elementPage);
+    // numberPage;
+    //lưu numberPage ra biến Global
+    let startPage = (numberPage * elementPage);
+    let endPage = ((numberPage + 1) * elementPage);
+    let subArr = data.slice(startPage, endPage);
+    showHome(subArr);
+    showFootPage();
+    console.log(totalPage)
+}
+
+function showFootPage() {
+    let content = `<div id="footPage">
+                     <button class="btn btn-outline-primary" id="previous" onclick="previousPage(numberPage)">Previous</button>
+                     <span>${numberPage + 1}/${totalPage}</span>
+                     <button class="btn btn-outline-primary" id="next" onclick="nextPage(numberPage)">Next</button>
+                     </div>`
+    document.getElementById("footPage").innerHTML = content;
+    if (numberPage === 0){
+        $("#previous").hide();
+    }else if (numberPage === totalPage - 1){
+        $("#next").hide();
+    }
+}
+
+function previousPage(page) {
+    numberPage = page - 1;
+    showPage();
+}
+
+function nextPage(page) {
+    numberPage = page + 1;
+    showPage();
+}
+
+function showHome(data) {
+    let content = ""
+    for (let i = 0; i < data.length; i++) {
+        content += `
+<div class="col-xl-3 col-lg-4 col-md-6">
+    <div class="product-item">
+        <p class="position-relative bg-light overflow-hidden">
+        <p id="\img${data[i].idHome}\"></p>
+           
+            <div class="bg-secondary rounded text-white position-absolute start-0 top-0 m-4 py-1 px-3">New</div>
+        </div>
+        <div class="text-center p-4">
+            <span class="d-block h5 mb-2">${data[i].name}</span>
+            <span class="text-primary me-1">Giá: ${data[i].price}</span>
+            <span class="text-body"> Phòng ngủ: ${data[i].bedroom_count}</span><br>
+            <span class="text-body"> Phòng tắm: ${data[i].bathroom_count}</span><br>
+            <span class="text-body"> Địa chỉ: ${data[i].address.name},${data[i].address.city.name}</span>
+        </div>
+        <div class="d-flex border-top">
+            <small class="w-50 text-center border-end py-2">
+                <a class="text-body" href=""><i class="fa fa-eye text-primary me-2"></i>Xem chi tiết</a>
+            </small>
+            <small class="w-50 text-center py-2">
+                <a class="text-body" href=""><i class="far fa-heart"></i>  Thuê ngay</a>
+            </small>
+        </div>
+    </div>
+</div>`
+        displayImg(data[i].idHome)
+    }
+    document.getElementById("homes").innerHTML = content
+}
+function displayImg(id) {
+    var settings = {
+        "url": `http://localhost:8080/api/homes/img/${id}`,
+        "method": "GET",
+        "timeout": 0,
+    };
+
+    $.ajax(settings).done(function (response) {
+        let content = "";
+        for (let i = 0; i < response.length; i++) {
+            content += `<img style="height: 200px"  src="../../src/main/resources/static/image/${response[i].image}" alt=""/>`
+            break;
+        }
+        document.getElementById("img" + id).innerHTML = content;
+    });
+}
+
 let arr;
 function displayAll1() {
     $.ajax({
@@ -174,18 +377,18 @@ function save() {
     document.getElementById("form").reset()
     event.preventDefault()
 }
-function displayImg(id) {
-    var settings = {
-        "url": `http://localhost:8080/api/homes/img/${id}`,
-        "method": "GET",
-        "timeout": 0,
-    };
-
-    $.ajax(settings).done(function (response) {
-        let content = "";
-        for (let i = 0; i < response.length; i++) {
-            content += `<img style="width: 100px" src="../../src/main/resources/static/image/${response[i].image}" alt=""/>`
-        }
-        document.getElementById("img" + id).innerHTML = content;
-    });
-}
+// function displayImg(id) {
+//     var settings = {
+//         "url": `http://localhost:8080/api/homes/img/${id}`,
+//         "method": "GET",
+//         "timeout": 0,
+//     };
+//
+//     $.ajax(settings).done(function (response) {
+//         let content = "";
+//         for (let i = 0; i < response.length; i++) {
+//             content += `<img style="width: 100px" src="../../src/main/resources/static/image/${response[i].image}" alt=""/>`
+//         }
+//         document.getElementById("img" + id).innerHTML = content;
+//     });
+// }
